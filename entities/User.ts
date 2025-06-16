a3b5e1afc7faa089a06e34bf22fe091dda3b85a2
Anthony Cursewl <zerpaanthony.wx@gmail.com>
2025-06-16 00:16:43 -0400

    feat: Implement user object and login functionality in auth store

---
+++ entities/User.ts
@@ -0,0 +1,9 @@
+export interface User {
+    id: string;
+    name: string;
+    username: string;
+    email: string;
+    role: string;
+    created_at: Date;
+    password: string;
+}
--- services/stores/auth/useLoginStore.ts
+++ services/stores/auth/useLoginStore.ts
@@ -2,6 +2,9 @@ import { secureFetch } from '@/services/http/secureFetch';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { create } from 'zustand';
 
+// entities
+import { User } from '@/entities/User';
+
 interface LoginStore {
     loading: boolean;
     setLoading: (loading: boolean) => void;
@@ -10,6 +13,9 @@ interface LoginStore {
     setError: (error: string | null) => void;
     isAuthenticated: boolean;
     setIsAuthenticated: (isAuthenticated: boolean) => void;
+    user: User;
+    setUser: (user: User) => void;
+    getUserDetails: () => Promise<User>;
 }
 
 export const useLoginStore = create<LoginStore>((set, get) => ({
@@ -19,11 +25,22 @@ export const useLoginStore = create<LoginStore>((set, get) => ({
     setError: (error) => set({ error }),
     isAuthenticated: false,
     setIsAuthenticated: (isAuthenticated) => set({ isAuthenticated }),
+    user: {} as User,
+    setUser: (user) => set({ user }),
+
+    /**
+     * Login function
+     * @param email 
+     * @param password 
+     * @returns 
+     */
     async login(email: string, password: string): Promise<boolean> {
         const { setLoading, setIsAuthenticated, setError } = get()
         setError(null) 
         const { data, error } = await secureFetch(`/auth/login`, {
             method: 'POST',
+            stringify: true,
+            content_type: 'application/json',
             body: { email: email.trim(), password: password.trim() }
         }, setLoading)
 
@@ -37,7 +54,6 @@ export const useLoginStore = create<LoginStore>((set, get) => ({
 
         if (data) {
             setIsAuthenticated(true)
-            console.log(data)
             await AsyncStorage.setItem('token', data.tokens.access_token)
             await AsyncStorage.setItem('refresh_token', data.tokens.refresh_token)
 
@@ -45,5 +61,36 @@ export const useLoginStore = create<LoginStore>((set, get) => ({
         }
 
         return false
+    },
+
+    /**
+     * Get user details
+     * @returns User
+    */
+    async getUserDetails(): Promise<User> {
+        const { setLoading, setIsAuthenticated, setError, setUser } = get()
+        setError(null) 
+        const { data, error } = await secureFetch(`/auth/verify`, {
+            method: 'GET',
+            content_type: 'application/json'
+        }, setLoading)
+
+        if (error) {
+            setIsAuthenticated(false)
+            setError(error)
+            console.log(error)
+
+            return {} as User
+        }
+
+        if (data) {
+            setIsAuthenticated(true)
+            setError(null)
+            setUser(data)
+
+            return data
+        }
+
+        return {} as User
     }
 }))
