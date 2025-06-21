e7b8ff431d392fc87bd3e291b3e89b36fc13de1d
Anthony Cursewl <zerpaanthony.wx@gmail.com>
2025-06-20 22:22:23 -0400

    feat: Update app colors in app.json and API base URL in anx.app.config.ts

--- config/anx.app.config.ts
+++ config/anx.app.config.ts
@@ -7,4 +7,4 @@
  * @type {string}
  * @default "http://192.168.0.103:3001"
  */
-export const API_BASE_URL = "http://192.168.0.107:3001"
+export const API_BASE_URL = "https://anx-api-streak.breadriuss.com"
